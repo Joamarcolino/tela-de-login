42a44d1998b217728608ae31bc4e5c37393ec52f
João Vitor Marcolino Veiga <166542665+Joamarcolino@users.noreply.github.com>
2024-11-11 15:39:03 -0300

    Css update 3
    epicness at its finest.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,33 @@
-import { useState } from 'react'
+import React from 'react';
 
 export default function Home() {
-
   return (
-    <div className='h-[100vh] flex  justify-center align-centers'>
-      <div className='  p-[20px]  size-auto flex flex-col align-center justify-center '>
-        <div className='opacity-80 bg-white rounded-3xl w-[400px]'>
-          <h2 className='flex justify-center mb-9 text-[30px] font-bold font-mono cursor-not-allowed  '>
-            Do your Login
-          </h2>
-          <div className=' flex flex-col items-center justify-center'>
-            <h1 className='text-[14px] font-sans font-mono cursor-not-allowed'>Email </h1>
-            <input className='border-gray-300 rounded-md bg-slate-400 font-sans text-[16px] outline-[40px] outline-cyan-500' type="text" placeholder='Enter your email' />
+    <div className="h-screen flex justify-center items-center">
+      <div className="p-5 flex flex-col items-center">
+        <div className="bg-[rgb(21,27,35)] rounded-lg w-[260px] border border-gray-800">
+          <div className="flex flex-col items-start p-2">
+            <label className="text-[14px] text-white font-sans">Email</label>
+            <input
+              className="w-full mt-2 p-2 border-2 border-gray-800 rounded-md bg-[rgb(13,17,23)] text-[13px] text-white outline-none"
+              type="email"
+              placeholder='Insert your gmail'
+            />
           </div>
-          <div className='mt-4 flex  flex-col items-center justify-center'>
-            <h1 className='text-[14px] font-bold font-mono cursor-not-allowed'>Password</h1>
-            <input className='text-red-500 border-gray-300 bg-slate-400 outline-[40px] outline-cyan-500 border-2 rounded-md  font-sans align-text-center text-[16px]' type="text" placeholder='Enter your password.' />
-            <div className='mt-5'>
-              <button className=' flex justify-center align-center border-2 rounded-2xl p-2 mb-1'>Finish</button>
-            </div>
-          </div >
+          <div className="flex flex-col items-start p-2">
+            <label className="text-[14px] text-white font-sans">Password</label>
+            <input
+              className="w-full mt-2 p-2 border-2 border-gray-800 rounded-md bg-[rgb(13,17,23)] text-[13px] text-white outline-blue-600"
+              type="password"
+              placeholder="Insert your password"
+            />
+          </div>
+          <div className="p-5">
+            <button className=" text-white w-full bg-green-600 rounded-lg p-2 text-[13px] hover:bg-green-700 active:bg-green-500">
+              Sign in
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
