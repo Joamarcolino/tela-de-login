64ec40b3ed71921e3d8cb71627cbf0097fef2ef3
João Vitor Marcolino Veiga <166542665+Joamarcolino@users.noreply.github.com>
2024-11-18 06:02:54 -0300

    Update 3 ( i think )
    Now the build works in some ways, though it really doesn't have a built in back-end or anything like that. Next time i will probally add it. For now, enjoy the lil things i did to it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,12 @@ export default function Home() {
   const [error, setError] = useState('');
 
 
-  const handleSubmit = (e) => {
+
+   //Serve para identificar e dar funcionalidades para os elementos email e password.
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!email || !password) {
-      setError(' ITS FOCKEN EMPTY M8');
+      setError('email or password not informed');
       return;
     }
     // Handle sign-in logic here
@@ -20,15 +22,26 @@ export default function Home() {
     setError('');
   };
 
+
+  //Serve para identificar quando alguém clicar no link para o video e manda um aviso caso sim.
+  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    if (window.confirm('Bro this is an experimental feature you sure you want to continue? Mb if you dont like it though.')) {
+      window.location.href = e.currentTarget.href;
+    }
+  };
+
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="p-5 flex flex-col items-center">
         <div className="bg-[rgb(21,27,35)] rounded-lg w-[260px] border-4 border-gray-800">
           <form onSubmit={handleSubmit}>
             <div className="flex flex-col items-start p-2">
-              <label className="text-[14px] text-white font-sans">Email address</label>
+              <div >
+                <label className="text-[14px] text-white font-sans">Email address</label>
+              </div>
               <input
-                className="w-full mt-1 p-2 border-2 border-gray-800 rounded-md bg-[rgb(13,17,23)] text-[13px] text-white outline-none"
+                className="w-full mt-1 p-2 border-2 border-gray-800 rounded-md bg-[rgb(13,17,23)] text-[13px] text-white outline-blue-600"
                 type="email"
                 placeholder="Insert your email"
                 value={email}
@@ -36,7 +49,12 @@ export default function Home() {
               />
             </div>
             <div className="flex flex-col items-start p-2">
-              <label className="text-[14px] text-white font-sans">Password</label>
+              <div className='flex space-x-12 '>
+                <label className="text-[14px] text-white font-sans">Password</label>
+                <a className=' text-blue-400 text-[11px] font-mono '
+                  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                  onClick={handleLinkClick} >Forgot password?</a>
+              </div>
               <input
                 className="w-full mt-1 p-2 border-2 border-gray-800 rounded-md bg-[rgb(13,17,23)] text-[13px] text-white outline-blue-600"
                 type="password"
